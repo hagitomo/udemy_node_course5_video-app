b1b9aa3afbf72a8e7f0cd83de559921e9cfb486f
Tomoyuki Hagiwara <hagiwara.t.tam@tam-tam.co.jp>
2019-02-15 00:32:05 +0900

    users/register/ validation

--- router/users.js
+++ router/users.js
@@ -11,4 +11,30 @@ router.get('/login/', (req, res) => {
 router.get('/register/', (req, res) => {
   res.render('users/register')
 })
+
+router.post('/register/', (req, res) => {
+  let errors = []
+
+  // password validation
+  if (req.body.password !== req.body.password2 ) {
+    errors.push({ text: 'passwords do not match' })
+  }
+  if(req.body.password.length < 4) {
+    errors.push({ text: 'passowrd must be at least 4 chars' })
+  }
+
+  if(errors.length > 0) {
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    })
+  } else {
+    res.send('pass')
+  }
+
+})
+
 module.exports = router
